b0a5d7e46ab2489cc5a8bedd3894062036fda13c
facundolachalde <facundo.lachalde182124@alumnos.info.unlp.edu.ar>
2022-12-08 18:22:18 -0600

    first commit

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const data = [
+	{
+		id: 1,
+		img: 'https://images.unsplash.com/photo-1603994236406-bf7f71dc361e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1951&q=80',
+		nameProduct: 'Skin 1',
+		price: 80,
+		quantity: 1,
+	},
+	{
+		id: 2,
+		img: 'https://images.unsplash.com/flagged/photo-1560177776-55a762c5c000?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80',
+		nameProduct: 'Skin 2',
+		price: 50,
+		quantity: 1,
+	},
+	{
+		id: 3,
+		img: 'https://images.unsplash.com/flagged/photo-1560177776-295b9cd779de?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=502&q=80',
+		nameProduct: 'Skih 3',
+		price: 90,
+		quantity: 1,
+	},
+	{
+		id: 4,
+		img: 'https://images.unsplash.com/photo-1637594439872-44d1b1fe0a0b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+		nameProduct: 'Skin 4',
+		price: 50,
+		quantity: 1,
+	},
+	{
+		id: 5,
+		img: 'https://images.unsplash.com/photo-1630534658718-395efda906cb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80',
+		nameProduct: 'Skih 5',
+		price: 20,
+		quantity: 1,
+	},
+];
